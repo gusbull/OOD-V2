90fdc801f28e40ed487c4056b7a31864fe35e1f3
gusbull <35735406+gusbull@users.noreply.github.com>
2020-04-14 17:31:02 +0700

    update
    update

---
+++ react-login-signup-ui-template/src/config.js
@@ -0,0 +1,15 @@
+const config = {
+
+apiKey: "AIzaSyDscUHgzFuKX7c5DOaYKRN8SjiDpydMRQI",
+authDomain: "web-application-for-students.firebaseapp.com",
+databaseURL: "https://web-application-for-students.firebaseio.com",
+projectId: "web-application-for-students",
+storageBucket: "web-application-for-students.appspot.com",
+messagingSenderId: "572309989654",
+appId: "1:572309989654:web:aeeba9c86ac3993b1ac837",
+measurementId: "G-3R1N1N5072"
+}
+
+
+
+export default config
